9a0cd213d3700f6dd02ac29d222e98325e01cf4e
Omar Mukadam <om@princeton.edu>
2017-03-27 20:11:42 -0400

    Updated packages.json and binded port to process.env.PORT or, if none available, port 8080

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var express = require('express');
 var server = express();
 server.use(express.static(__dirname + '/public'));
 
-var port = 8080;
+// Bind port to process.env.PORT or, if none available, port 8080
+var port = process.env.PORT || 8080;
 server.listen(port, function() {
     console.log('server listening on port ' + port);
 });
