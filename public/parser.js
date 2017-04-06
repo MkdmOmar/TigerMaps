bc0f91b52acfa3bdcc338271a2e4fabea37e98f4
Omar Mukadam <om@princeton.edu>
2017-04-06 12:00:44 -0400

    Updated parser to retrieve data from all relevants feeds in the OIT Web Feed Registry.

--- public/parser.js
+++ public/parser.js
@@ -35,9 +35,55 @@ function parse(webFeedURL, returnResult) {
 }
 
 
+// Computing and Printing: Provides XML streams of data about computer and
+// printing public clusters on campus
+compPrintURL = "https://etcweb.princeton.edu/webfeeds/places/?categoryID=5"
 
+// Dining: Provides XML streams of data about dining facilities on campus.
+diningURL = "https://etcweb.princeton.edu/webfeeds/places/?categoryID=20"
+
+// Locations: Provides XML and JSON streams of location data for buildings,
+// parking lots and other constructed elements of the Princeton University campus.
+locationsURL = "https://etcweb.princeton.edu/webfeeds/map/"
+
+// Places: Provides XML streams of data about "places" on campus
+// (e.g. dining, printing, parking, etc.)
+placesURL = "http://etcweb.princeton.edu/webfeeds/places/"
+
+// Public Events: Provides near-real-time XML and JSON streams of event data
+// from the R25 public events scheduling system.
 publicEventsURL = "https://etcweb.princeton.edu/webfeeds/events/"
 
+// USG Events: Provides iCal and XML streams of event data from the USG Student
+// Events Calendar.
+USGEventsURL = "https://etcweb.princeton.edu/webfeeds/events/usg/"
+
+parse(compPrintURL, function(compPrintXML){
+  console.log("Retrieved Computing and Printing Data!")
+  // console.log(compPrintXML);
+});
+
+parse(diningURL, function(diningXML){
+  console.log("Retrieved Dining Data!")
+  // console.log(diningXML);
+});
+
+parse(locationsURL, function(locationsXML){
+  console.log("Retrieved Locations Data!")
+  // console.log(locationsXML);
+});
+
+parse(placesURL, function(placesXML){
+  console.log("Retrieved Places Data!")
+  // console.log(placesXML);
+});
+
 parse(publicEventsURL, function(publicEventsXML){
-  console.log(publicEventsXML);
+  console.log("Retrieved Public Events Data!")
+  // console.log(publicEventsXML);
+});
+
+parse(USGEventsURL, function(USGEventsXML){
+  console.log("Retrieved USG Events Data!")
+  // console.log(USGEventsXML);
 });
