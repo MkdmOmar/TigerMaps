dd17b1957655961764105769f0c044f67a6e4c68
Omar Mukadam <om@princeton.edu>
2017-04-06 11:08:25 -0400

    Implemented parser for public events data feed. Retrieves data in XML format.

--- public/parser.js
+++ public/parser.js
@@ -1,10 +1,33 @@
-var x = new XMLHttpRequest();
-x.open("GET", "http://feed.example/", true);
-x.onreadystatechange = function () {
-  if (x.readyState == 4 && x.status == 200)
+/*
+BEFORE USING THIS:
+
+Make sure you allow requests to any site with ajax from any source.
+
+Use this Chrome extension:
+https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi?hl=en
+It adds to response 'Allow-Control-Allow-Origin: *' header
+
+This is just for development purposes only.
+*/
+
+
+console.log("loading parser!")
+
+publicEventsURL = "https://etcweb.princeton.edu/webfeeds/events/"
+var pubEventsReq = new XMLHttpRequest();
+pubEventsReq.open("GET", publicEventsURL, true);
+pubEventsReq.onreadystatechange = function () {
+  // If request was a success
+  if (pubEventsReq.readyState == 4 && pubEventsReq.status == 200)
   {
-    var doc = x.responseXML;
-    // …
+    // Parse XML as string
+    var pubEvents = new XMLSerializer().serializeToString(pubEventsReq.responseXML)
+    console.log("parser success!")
+    console.log(pubEvents);
+
+  }
+  else {
+    console.log("parser failure!")
   }
 };
-x.send(null);
+pubEventsReq.send(null);
