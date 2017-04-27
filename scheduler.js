338af202b808fa9891427ff55f660ccaaf281dce
Jose M Rico <jrico@princeton.edu>
2017-04-27 01:00:37 -0400

    scheduler

--- scheduler.js
+++ scheduler.js
@@ -1,12 +1,15 @@
 const cron = require("node-cron");
 
-var updateFreq = "0 * 0 * * *";
+// Frequency of database update.
+// Look up "Cron format" and this will make sense.
+var updateFreq = "0 * * * *";
 
 function updateDatabase() {
-  console.log("update database!");
+  console.log("Scheduler: updated database");
 }
 
 function start() {
+  updateDatabase();
   cron.schedule(updateFreq, updateDatabase);
 }
 
