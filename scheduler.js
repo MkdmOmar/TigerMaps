a69d58480dff17c19438fede89ddffdca4f473e3
Jose M Rico <jrico@princeton.edu>
2017-05-14 12:52:59 -0400

    Clear database every day

--- scheduler.js
+++ scheduler.js
@@ -2,18 +2,25 @@ const cron = require("node-cron");
 
 const parser = require("./parser");
 
-// Frequency of database update.
+// Frequencies of database update and clear.
 // Look up "Cron format" and this will make sense.
-var updateFreq = "0 * * * *";
+var updateFreq  = "0 * * * *";
+var clearFreq   = "* 0 * * *"
 
 function updateDatabase() {
     console.log("Scheduler: updated database");
     parser.updateDB();
 }
 
+function clearDatabase() {
+    console.log("Scheduler: cleared database");
+    parser.clearDB();
+}
+
 function start() {
     updateDatabase();
     cron.schedule(updateFreq, updateDatabase);
+    cron.schedule(clearFreq, clearDatabase);
 }
 
 module.exports = {
